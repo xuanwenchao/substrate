8828b388280c3737d675f4a8696053c99f092691
xuanwenchao <407300101@qq.com>
2023-03-22 10:58:09 +0800

    Update Lesson4_homework.rs

--- Lesson4_homework.rs
+++ Lesson4_homework.rs
@@ -33,5 +33,30 @@ fn main() {
     println!("yellow time:{}",yellow.get_time_seconds());
 }
 
+/*
+* 2.实现一个函数，为u32类型的整数集合求和，参数类型为&[u32]，返回类型为Option,溢出时返回None
+*/
+use std::{option::Option};
+fn get_sum(nums: &[u32]) -> Option<u32> {
+    let mut sum: u32 = 0;
+    for val in nums {
 
+        let result = sum.checked_add(*val);
+        match result {
+            Some(_) => sum+=val,
+            None => return None,
+        }
+    }
+    return Option::Some(sum);
+}
+
+fn main() {
+    let arr:Vec<u32> = vec![19, 23, 200, 12, 30];
+    let res = get_sum(&arr);
+    match res {
+        Some(r) => println!("{}", r),
+        None => println!("overflow!"),
+    }
+    
+}
 
